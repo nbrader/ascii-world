63ac03c01e1d099ad4ff158b7e344b831f23d53d
nbrader <nbrader+github@gmail.com>
2024-12-03 22:21:15 +0000

    Completed day 1

--- day01 - Historian Hysteria/day1.hs
+++ day01 - Historian Hysteria/day1.hs
@@ -15,10 +15,10 @@
 -- Output --
 ------------
 -- *Main> day1part1
--- 
+-- 1651298
 
 -- *Main> day1part2
--- 
+-- 21306195
 
 
 -------------
@@ -33,7 +33,7 @@ import Data.Maybe (fromJust)
 -------------
 -- Program --
 -------------
-main = day1part1
+main = day1part2
 
 day1part1 = do
   contents <- readFile "day1 (data).csv"
@@ -42,7 +42,10 @@ day1part1 = do
   let [col1,col2] = sortedColumnsOfWords
   print $ sum . map abs $ zipWith (-) col2 col1
 
--- day1part2 = do
-  -- contents <- readFile "day1 (data).csv"
-  -- let total = sum . map lineToNum . lines $ contents
-  -- print $ total
+day1part2 = do
+  contents <- readFile "day1 (example).csv"
+  let rowsOfWords = map words $ lines contents
+  let sortedColumnsOfWords = map sort . map (map ((read :: String -> Int))) $ transpose rowsOfWords
+  let [col1,col2] = sortedColumnsOfWords
+  let occurrences = map (\val -> length $ filter (==val) col2) col1
+  print $ sum $ zipWith (*) col1 occurrences
