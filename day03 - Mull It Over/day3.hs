ab6c90846905dff5317c584c9c5b0fd6123ec4a5
nbrader <nbrader+github@gmail.com>
2024-12-04 01:03:24 +0000

    Committing attempt at part 2 of day 3 (doesn't yet work)

--- day03 - Mull It Over/day3.hs
+++ day03 - Mull It Over/day3.hs
@@ -35,66 +35,117 @@ import Data.Maybe (fromJust)
 -------------
 main = day3part1
 
-data ParserState = SeachingForMOrD | MulDisabled | MFound | UFound | LFound | OpenBracFound | ArgStr1DigitFound | CommaFound | ArgStr2DigitFound
+data ParserState = SeachingForMulOrDont | MulDisabledSearchingForDo | DFoundOfDo | OFoundOfDo | OpenBracFoundOfDo | DFoundOfDont | OFoundOfDont | NFoundOfDont | AposFoundOfDont | TFoundOfDont | OpenBracFoundOfDont | MFoundOfMul | UFoundOfMul | LFoundOfMul | OpenBracFoundOfMul | ArgStr1DigitFoundOfMul | CommaFoundOfMul | ArgStr2DigitFoundOfMul
 data Expr = Mul Expr Expr | Plus Expr Expr | Val Int deriving (Show)
 
 parse :: String -> Expr
-parse s = go SeachingForMOrD s "" ""
+parse s = go SeachingForMulOrDont s "" ""
   where
     go :: ParserState -> String -> String -> String -> Expr
     go _ [] _ _
         = Val 0
     
-    go SeachingForMOrD (x:xs) argStr1 argStr2
+    go SeachingForMulOrDont (x:xs) argStr1 argStr2
         = case x of
-            'm' -> go MFound xs argStr1 argStr2
-            _   -> go SeachingForMOrD xs "" ""
+            'm' -> go MFoundOfMul xs argStr1 argStr2
+            'd' -> go DFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
     
-    go MFound (x:xs) argStr1 argStr2
+    go DFoundOfDont (x:xs) argStr1 argStr2
         = case x of
-            'u' -> go UFound xs argStr1 argStr2
-            _   -> go SeachingForMOrD xs "" ""
+            'o' -> go OFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
     
-    go UFound (x:xs) argStr1 argStr2
+    go OFoundOfDont (x:xs) argStr1 argStr2
         = case x of
-            'l' -> go LFound xs argStr1 argStr2
-            _   -> go SeachingForMOrD xs "" ""
+            'n' -> go NFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
     
-    go LFound (x:xs) argStr1 argStr2
+    go NFoundOfDont (x:xs) argStr1 argStr2
         = case x of
-            '(' -> go OpenBracFound xs argStr1 argStr2
-            _   -> go SeachingForMOrD xs "" ""
+            '\'' -> go AposFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
     
-    go OpenBracFound (x:xs) argStr1 argStr2
+    go AposFoundOfDont (x:xs) argStr1 argStr2
+        = case x of
+            't' -> go TFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go TFoundOfDont (x:xs) argStr1 argStr2
+        = case x of
+            '(' -> go OpenBracFoundOfDont xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go OpenBracFoundOfDont (x:xs) argStr1 argStr2
+        = case x of
+            ')' -> go MulDisabledSearchingForDo xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go MulDisabledSearchingForDo (x:xs) argStr1 argStr2
+        = case x of
+            'd' -> go DFoundOfDo xs argStr1 argStr2
+            _   -> go MulDisabledSearchingForDo xs "" ""
+    
+    go DFoundOfDo (x:xs) argStr1 argStr2
+        = case x of
+            'o' -> go OFoundOfDo xs argStr1 argStr2
+            _   -> go MulDisabledSearchingForDo xs "" ""
+    
+    go OFoundOfDo (x:xs) argStr1 argStr2
+        = case x of
+            '(' -> go OpenBracFoundOfDo xs argStr1 argStr2
+            _   -> go MulDisabledSearchingForDo xs "" ""
+    
+    go OpenBracFoundOfDo (x:xs) argStr1 argStr2
+        = case x of
+            ')' -> go OpenBracFoundOfDo xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go MFoundOfMul (x:xs) argStr1 argStr2
+        = case x of
+            'u' -> go UFoundOfMul xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go UFoundOfMul (x:xs) argStr1 argStr2
+        = case x of
+            'l' -> go LFoundOfMul xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go LFoundOfMul (x:xs) argStr1 argStr2
+        = case x of
+            '(' -> go OpenBracFoundOfMul xs argStr1 argStr2
+            _   -> go SeachingForMulOrDont xs "" ""
+    
+    go OpenBracFoundOfMul (x:xs) argStr1 argStr2
         = if isDigit x
-            then go ArgStr1DigitFound xs (argStr1++[x]) argStr2
-            else go SeachingForMOrD xs "" ""
+            then go ArgStr1DigitFoundOfMul xs (argStr1++[x]) argStr2
+            else go SeachingForMulOrDont xs "" ""
     
-    go ArgStr1DigitFound (x:xs) argStr1 argStr2
+    go ArgStr1DigitFoundOfMul (x:xs) argStr1 argStr2
         = if isDigit x
-            then go ArgStr1DigitFound xs (argStr1++[x]) argStr2
+            then go ArgStr1DigitFoundOfMul xs (argStr1++[x]) argStr2
             else if x == ','
-                    then go CommaFound xs argStr1 argStr2
-                    else go SeachingForMOrD xs "" ""
+                    then go CommaFoundOfMul xs argStr1 argStr2
+                    else go SeachingForMulOrDont xs "" ""
     
-    go CommaFound (x:xs) argStr1 argStr2
+    go CommaFoundOfMul (x:xs) argStr1 argStr2
         = if isDigit x
-            then go ArgStr2DigitFound xs argStr1 (argStr2++[x])
-            else go SeachingForMOrD xs "" ""
+            then go ArgStr2DigitFoundOfMul xs argStr1 (argStr2++[x])
+            else go SeachingForMulOrDont xs "" ""
     
-    go ArgStr2DigitFound (x:xs) argStr1 argStr2
+    go ArgStr2DigitFoundOfMul (x:xs) argStr1 argStr2
         = if isDigit x
-            then go ArgStr2DigitFound xs argStr1 (argStr2++[x])
+            then go ArgStr2DigitFoundOfMul xs argStr1 (argStr2++[x])
             else if x == ')'
-                    then Plus (Mul (Val $ read argStr1) (Val $ read argStr2)) (go SeachingForMOrD xs "" "")
-                    else go SeachingForMOrD xs "" ""
+                    then Plus (Mul (Val $ read argStr1) (Val $ read argStr2)) (go SeachingForMulOrDont xs "" "")
+                    else go SeachingForMulOrDont xs "" ""
 
 evaluate :: Expr -> Int
 evaluate (Mul x y) = evaluate x * evaluate y
 evaluate (Plus x y) = evaluate x + evaluate y
 evaluate (Val x) = x
 
-day3part1 = do
-  contents <- readFile "day3 (data).csv"
+day3part2 = do
+  contents <- readFile "day3 (example).csv"
   let result = evaluate $ parse contents
   print result
