bf81cee6f0d37b7bb915ac037e8a3209cfbb431e
nbrader <nbrader+github@gmail.com>
2024-12-04 14:20:32 +0000

    Saved both parts of day 3 alongside each other

--- day03 - Mull It Over/day3.hs
+++ day03 - Mull It Over/day3.hs
@@ -15,10 +15,10 @@
 -- Output --
 ------------
 -- *Main> day3part1
--- 
+-- 167090022
 
 -- *Main> day3part2
--- 
+-- 89823704
 
 
 -------------
@@ -38,8 +38,39 @@ main = day3part2
 data ParserState = SeachingForMulOrDont | MulDisabledSearchingForDo | UpToOpenBracFoundOfMul | ArgStr1DigitFoundOfMul | CommaFoundOfMul | ArgStr2DigitFoundOfMul
 data Expr = Mul Expr Expr | Plus Expr Expr | Val Integer deriving (Show)
 
-parse :: String -> Expr
-parse s = go SeachingForMulOrDont s "" ""
+parse1 :: String -> Expr
+parse1 s = go SeachingForMulOrDont s "" ""
+  where
+    go :: ParserState -> String -> String -> String -> Expr
+    go _ [] _ _
+        = Val 0
+    go SeachingForMulOrDont xs@(x:xsTail) argStr1 argStr2
+        = if and (zipWith (==) "mul(" xs)
+            then go UpToOpenBracFoundOfMul (drop (length "mul(") xs) argStr1 argStr2
+            else go SeachingForMulOrDont xsTail "" ""
+    go UpToOpenBracFoundOfMul xs@(x:xsTail) argStr1 argStr2
+        = if isDigit x
+            then go ArgStr1DigitFoundOfMul xsTail (argStr1++[x]) argStr2
+            else go SeachingForMulOrDont xsTail "" ""
+    go ArgStr1DigitFoundOfMul xs@(x:xsTail) argStr1 argStr2
+        = if isDigit x
+            then go ArgStr1DigitFoundOfMul xsTail (argStr1++[x]) argStr2
+            else if x == ','
+                    then go CommaFoundOfMul xsTail argStr1 argStr2
+                    else go SeachingForMulOrDont xsTail "" ""
+    go CommaFoundOfMul xs@(x:xsTail) argStr1 argStr2
+        = if isDigit x
+            then go ArgStr2DigitFoundOfMul xsTail argStr1 (argStr2++[x])
+            else go SeachingForMulOrDont xsTail "" ""
+    go ArgStr2DigitFoundOfMul xs@(x:xsTail) argStr1 argStr2
+        = if isDigit x
+            then go ArgStr2DigitFoundOfMul xsTail argStr1 (argStr2++[x])
+            else if x == ')'
+                    then Plus (Mul (Val $ read argStr1) (Val $ read argStr2)) (go SeachingForMulOrDont xsTail "" "")
+                    else go SeachingForMulOrDont xsTail "" ""
+
+parse2 :: String -> Expr
+parse2 s = go SeachingForMulOrDont s "" ""
   where
     go :: ParserState -> String -> String -> String -> Expr
     go _ [] _ _
@@ -88,7 +119,12 @@ evaluate (Mul x y) = evaluate x * evaluate y
 evaluate (Plus x y) = evaluate x + evaluate y
 evaluate (Val x) = x
 
+day3part1 = do
+  contents <- readFile "day3 (data).csv"
+  let result = parse1 contents
+  print $ evaluate result
+
 day3part2 = do
   contents <- readFile "day3 (data).csv"
-  let result = parse contents
+  let result = parse2 contents
   print $ evaluate result
