95226fcf4c0fdd1b9c2b3ad7831a369265a4a3d6
nbrader <nbrader+github@gmail.com>
2024-12-10 21:58:25 +0000

    Completed day 10

--- day10 - Hoof It/day10.hs
+++ day10 - Hoof It/day10.hs
@@ -35,7 +35,7 @@ import qualified Data.HashMap.Strict as Map
 -------------
 -- Program --
 -------------
-main = day10part1
+main = day10part2
 
 day10part1 = do
     contents <- readFile "day10 (data).csv"
@@ -52,6 +52,27 @@ day10part1 = do
     
     -- mapM_ (\k -> print $ fromJust $ Map.lookup k countTrailHeads) (sort $ Map.keys countTrailHeads)
     print $ sum (Map.elems countTrailHeads)
+
+day10part2 = do
+    contents <- readFile "day10 (data).csv"
+    
+    let lines = Prelude.lines $ contents
+        width  = length (head lines)
+        height = length lines
+        grid = listArray ((0,0),(width-1,height-1)) $ map digitToInt $ concat lines
+        
+        (tops:lowerLevelSets) = [[(x,y) | x <- [0..width-1], y <- [0..height-1], grid A.! (x,y) == i] | i <- [9,8..0]]
+        finishSets = do
+            top <- tops
+            return $ foldl step [[top]] lowerLevelSets
+          where step :: [[(Int, Int)]] -> [(Int, Int)] -> [[(Int, Int)]]
+                step upperSets next = concatMap step' upperSets
+                  where step' :: [(Int, Int)] -> [[(Int, Int)]]
+                        step' upper = map step'' (expand upper)
+                          where step'' :: (Int, Int) -> [(Int, Int)]
+                                step'' x = [x] `intersect` next
+    
+    print $ length $ concat $ concat $ finishSets
 
 expand :: [(Int,Int)] -> [(Int,Int)]
 expand = nub . concatMap (\p -> [p `addV2` v | v <- [(1,0),(0,1),(-1,0),(0,-1)]])
