bbb42cbcdf44fd0797d3c270223430d952ec7ab6
nbrader <nbrader+github@gmail.com>
2025-01-25 20:37:29 +0000

    Added notes on: - giving layers and points string names rather than char names - pseudo code for a partitioner algorithm

--- day12 - Garden Groups/BWorld.hs
+++ day12 - Garden Groups/BWorld.hs
@@ -1,6 +1,16 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-21.22 ghci --package containers-0.6.7 --package split-0.2.3.5 --package safe-0.3.19 --package QuickCheck-2.14.3
 
+-- To Do:
+-- Make bit masks and points named by strings instead of chars (though still using only first char for printing)
+-- Make partitioner which gives every disconnected part of a layer it's own name by tacking on the next number not already existing in the bit mask map
+--      Have the disconnected portions found by a flood fill algorithm using bit mask operations:
+--          Loop until all points in world have been sufficiently checked to account for all part of all layers (need a nice way of checking this)
+--              find an active point from the layer not so far a member of any partition part
+--              Loop until latest found points are empty
+--                  find new points by 'and'ing the latest found points in shifted up, down, left and right positions with the "visited" bit mask and 'or'ing them together
+--                  xor these points (to subtract them) from the "visited" bit mask and make them the new "latest found points"
+
 module BWorld ( BWorld(..)
               , emptyBWorld
               , readBWorld
